214d1c2c7773b504301065534e1437305a279ad1
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2021-05-21 15:29:37 +0200

    Add Xamarin.Forms app

---
+++ MobileFlyoutApp/XamarinFormsFlyoutApp/XamarinFormsFlyoutApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinFormsFlyoutApp.ViewModels;
+
+namespace XamarinFormsFlyoutApp.Views
+{
+  public partial class ItemDetailPage : ContentPage
+  {
+    public ItemDetailPage()
+    {
+      InitializeComponent();
+      BindingContext = new ItemDetailViewModel();
+    }
+  }
+}
---
+++ MobileFlyoutApp/XamarinFormsFlyoutApp/XamarinFormsFlyoutApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinFormsFlyoutApp.Models;
+using XamarinFormsFlyoutApp.ViewModels;
+
+namespace XamarinFormsFlyoutApp.Views
+{
+  public partial class NewItemPage : ContentPage
+  {
+    public Item Item { get; set; }
+
+    public NewItemPage()
+    {
+      InitializeComponent();
+      BindingContext = new NewItemViewModel();
+    }
+  }
+}
